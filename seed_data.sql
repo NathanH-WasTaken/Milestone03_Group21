f1c84c9ab6f812ddc9b1bbcc93a8507b30debfe5
Nathan Hilario <yielnh@icloud.com>
2026-03-06 14:17:32 -0800

    Student Inserts

--- seed_data.sql
+++ seed_data.sql
@@ -1,15 +1,23 @@
 /* Terms, Sets, Students */
 
+---------------------------------------
+-- TERMS
+---------------------------------------
 INSERT INTO TERMS VALUES ("202510", "Winter 2025", 2025-01-06, 2025-05-23);
 INSERT INTO TERMS VALUES ("202520", "Spring/Summer 2025", 2025-05-25, 2025-06-20);
 INSERT INTO TERMS VALUES ("202530", "Fall 2025", 2025-09-02, 2025-12-12);
 
+
+---------------------------------------
+-- SETS
+---------------------------------------
 INSERT INTO SETS VALUES ("A", "Burnaby");
 INSERT INTO SETS VALUES ("B", "Burnaby");
 INSERT INTO SETS VALUES ("C", "Burnaby");
 INSERT INTO SETS VALUES ("D", "Burnaby");
 INSERT INTO SETS VALUES ("E", "Downtown");
 INSERT INTO SETS VALUES ("F", "Downtown");
+
 
 ---------------------------------------
 -- COURSES
@@ -29,3 +37,27 @@ VALUES (u_ta1, Daniel Saavedra, ta, dsaavedra@bcit.ca);
 
 INSERT INTO users (user_id, display_name, role, email)
 VALUES (u_system, Lab Tracker System , system, noreply@labtracker.local);
+
+
+---------------------------------------
+-- STUDENTS
+---------------------------------------
+INSERT INTO STUDENTS VALUES ("A001", "A", "Ava", "Nguyen", "ava.nguyen@my.bcit.ca");
+INSERT INTO STUDENTS VALUES ("A002", "A", "Noah", "Kim", "noah.kim@my.bcit.ca");
+INSERT INTO STUDENTS VALUES ("A003", "A", "Oliver", "Singh", "oliver.singh@my.bcit.ca");
+INSERT INTO STUDENTS VALUES ("B001", "B", "Maya", "Fisher", "maya.fisher@my.bcit.ca");
+INSERT INTO STUDENTS VALUES (PLACEHOLDER);
+INSERT INTO STUDENTS VALUES (PLACEHOLDER);
+INSERT INTO STUDENTS VALUES (PLACEHOLDER);
+INSERT INTO STUDENTS VALUES (PLACEHOLDER);
+INSERT INTO STUDENTS VALUES (PLACEHOLDER);
+INSERT INTO STUDENTS VALUES (PLACEHOLDER);
+INSERT INTO STUDENTS VALUES (PLACEHOLDER);
+INSERT INTO STUDENTS VALUES (PLACEHOLDER);
+INSERT INTO STUDENTS VALUES (PLACEHOLDER);
+INSERT INTO STUDENTS VALUES (PLACEHOLDER);
+INSERT INTO STUDENTS VALUES (PLACEHOLDER);
+INSERT INTO STUDENTS VALUES (PLACEHOLDER);
+INSERT INTO STUDENTS VALUES (PLACEHOLDER);
+INSERT INTO STUDENTS VALUES (PLACEHOLDER);
+
